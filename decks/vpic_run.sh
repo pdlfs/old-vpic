d7c18dac92fe8acad5236b20641793d27dbb0675
George Amvrosiadis <gamvrosi@cs.cmu.edu>
2017-01-29 01:30:07 -0500

    minor fixes to run script

--- decks/vpic_run.sh
+++ decks/vpic_run.sh
@@ -3,9 +3,9 @@
 NODES=4
 CORES=16
 
-build_dir="/users/gamvrosi/src/vpic"
-deck_dir="/users/gamvrosi/src/vpic/decks/trecon-part"
-output_dir="/users/gamvrosi/src/vpic/decks/dump"
+build_dir="$HOME/src/vpic"
+deck_dir="$HOME/src/vpic/decks/trecon-part"
+output_dir="$HOME/src/vpic/decks/dump"
 
 die () { echo "Error: $@" 1>&2; exit 1;  }
 
@@ -14,10 +14,9 @@ fqdn_suffix="`hostname | sed 's/^[^\.]*././'`"
 exp_hosts="`/share/testbed/bin/emulab-listall`"
 
 echo $exp_hosts | awk -F, '{
-    for (i=1; i<=NF; i++)
-        print $i"'"$fqdn_suffix"'"
-    }' > $output_dir/vpic.hosts || \
-    die "failed to create vpic.hosts file"
+for (i=1; i<=NF; i++)
+    print $i"'"$fqdn_suffix"'"
+}' > "$output_dir/vpic.hosts" || die "failed to create vpic.hosts file"
 
 dpoints=10
 p=$CORES
@@ -34,13 +33,15 @@ do
         sed 's/VPIC_TOPOLOGY_Z.*/VPIC_TOPOLOGY_Z 1/' | \
         sed 's/VPIC_PARTICLE_X.*/VPIC_PARTICLE_X '$p'/' | \
         sed 's/VPIC_PARTICLE_Y.*/VPIC_PARTICLE_Y '$p'/' | \
-        sed 's/VPIC_PARTICLE_Z.*/VPIC_PARTICLE_Z 1/' | > $deck_dir/config.h || \
+        sed 's/VPIC_PARTICLE_Z.*/VPIC_PARTICLE_Z 1/' > $deck_dir/config.h || \
         die "config.h editing failed"
 
     # Compile input deck
-    $build_dir/build.op $deck_dir/turbulence.cxx || die "compilation failed"
+    cd $deck_dir || die "cd failed"
+    $build_dir/build.op ./turbulence.cxx || die "compilation failed"
 
     # Run VPIC experiment
+    cd $output_dir || die "cd failed"
     mkdir "$output_dir/run_$p" || die "mkdir failed"
     cd $output_dir/run_$p || die "cd failed"
 
