a315dc9be41b5c3f6e67b4c0d13ae93deca9e73d
George Amvrosiadis <gamvrosi@cs.cmu.edu>
2017-02-02 04:37:47 -0500

    Rewrite vpic_run.sh script

--- decks/vpic_run.sh
+++ decks/vpic_run.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# TODO: infer parameters in emulab, set in cray
-# TODO: deltafs server IP should not be hardcoded in script
-# TODO: add deltafs server code for mpich
-
 # Tunable parameters: tweak to your liking
 NODES=16
 CORES=64
@@ -15,11 +11,15 @@ ip_subnet="10.92"
 build_op_dir="$umbrella_build_dir/vpic-prefix/src/vpic-build"
 deck_dir="$umbrella_build_dir/vpic-prefix/src/vpic/decks/trecon-part"
 dpoints=7
+logfile=""
 
-die () { echo "Error: $@" 1>&2; exit 1;  }
+message () { echo "$@" | tee $logfile; }
+die () { message "Error $@"; exit 1; }
 
 # Generate mpirun hostfile on Narwhal
 gen_hosts() {
+    message "Generating hostfile..."
+
     fqdn_suffix="`hostname | sed 's/^[^\.]*././'`"
     exp_hosts="`/share/testbed/bin/emulab-listall`"
 
@@ -30,121 +30,179 @@ gen_hosts() {
 
 # Clear node caches on Narwhal
 clear_caches() {
-    /share/testbed/bin/emulab-mpirunall sudo sh -c 'echo 3 > /proc/sys/vm/drop_caches && free -m'
+    message "Clearing caches..."
+
+    /share/testbed/bin/emulab-mpirunall sudo sh -c \
+        'echo 3 > /proc/sys/vm/drop_caches'
 }
 
-gen_hosts
-
-p=$CORES
-while [ $dpoints -gt 0 ]
-do
-    clear_caches
-
-    echo "Running VPIC on $CORES cores, with $(( p * p * 100 )) particles."
-
-    # Configure VPIC experiment
+# Configure config.h
+# @1 in {"file-per-process", "file-per-particle"}
+build_deck() {
     cd $deck_dir || die "cd failed"
     mv $deck_dir/config.h $deck_dir/config.bkp || die "mv failed"
-    cat $deck_dir/config.bkp | \
-        sed 's/^#define VPIC_FILE_PER_PARTICLE/\/\/#define VPIC_FILE_PER_PARTICLE/' | \
-        sed 's/VPIC_TOPOLOGY_X.*/VPIC_TOPOLOGY_X '$CORES'/' | \
-        sed 's/VPIC_TOPOLOGY_Y.*/VPIC_TOPOLOGY_Y 1/' | \
-        sed 's/VPIC_TOPOLOGY_Z.*/VPIC_TOPOLOGY_Z 1/' | \
-        sed 's/VPIC_PARTICLE_X.*/VPIC_PARTICLE_X '$p'/' | \
-        sed 's/VPIC_PARTICLE_Y.*/VPIC_PARTICLE_Y '$p'/' | \
-        sed 's/VPIC_PARTICLE_Z.*/VPIC_PARTICLE_Z 1/' > $deck_dir/config.h || \
-        die "config.h editing failed"
+
+    case $1 in
+    "file-per-process")
+        cat $deck_dir/config.bkp | \
+            sed 's/^#define VPIC_FILE_PER_PARTICLE/\/\/#define VPIC_FILE_PER_PARTICLE/' | \
+            sed 's/VPIC_TOPOLOGY_X.*/VPIC_TOPOLOGY_X '$CORES'/' | \
+            sed 's/VPIC_TOPOLOGY_Y.*/VPIC_TOPOLOGY_Y 1/' | \
+            sed 's/VPIC_TOPOLOGY_Z.*/VPIC_TOPOLOGY_Z 1/' | \
+            sed 's/VPIC_PARTICLE_X.*/VPIC_PARTICLE_X '$p'/' | \
+            sed 's/VPIC_PARTICLE_Y.*/VPIC_PARTICLE_Y '$p'/' | \
+            sed 's/VPIC_PARTICLE_Z.*/VPIC_PARTICLE_Z 1/' > $deck_dir/config.h || \
+            die "config.h editing failed"
+            ;;
+    "file-per-particle")
+        cat $deck_dir/config.bkp | \
+            sed 's/^\/\/#define VPIC_FILE_PER_PARTICLE/#define VPIC_FILE_PER_PARTICLE/' | \
+            sed 's/VPIC_TOPOLOGY_X.*/VPIC_TOPOLOGY_X '$CORES'/' | \
+            sed 's/VPIC_TOPOLOGY_Y.*/VPIC_TOPOLOGY_Y 1/' | \
+            sed 's/VPIC_TOPOLOGY_Z.*/VPIC_TOPOLOGY_Z 1/' | \
+            sed 's/VPIC_PARTICLE_X.*/VPIC_PARTICLE_X '$p'/' | \
+            sed 's/VPIC_PARTICLE_Y.*/VPIC_PARTICLE_Y '$p'/' | \
+            sed 's/VPIC_PARTICLE_Z.*/VPIC_PARTICLE_Z 1/' > $deck_dir/config.h || \
+            die "config.h editing failed"
+            ;;
+    esac
 
     # Compile input deck
     cd $deck_dir || die "cd failed"
     $build_op_dir/build.op ./turbulence.cxx || die "compilation failed"
+}
 
-    # Run VPIC experiment
-    cd $output_dir || die "cd failed"
-    mkdir "$output_dir/baseline_$p" || die "mkdir failed"
-    cd $output_dir/baseline_$p || die "cd failed"
+# Run MPICH or OpenMPI mpirun command
+# Arguments:
+# @1 number of processes
+# @2 array of env vars: ("name1", "val1", "name2", ... )
+# @3 other options (to be appended)
+# @4 executable
+# @5 outfile
+do_mpirun() {
+    procs=$1
+    declare -a envs=("${!2}")
+    opts="$3"
+    exe="$4"
+    outfile="$5"
 
-    # TODO: Fix for mpich, openmpi, aprun
-    mpirun -np $CORES -npernode $(( CORES / NODES )) \
-        --hostfile $output_dir/vpic.hosts \
-        $deck_dir/turbulence.op 2>&1 | tee "$output_dir/baseline_$p.log" || \
-        die "run failed"
-
-    echo -n "Output size: " >> "$output_dir/baseline_$p.log"
-    du -b $output_dir/baseline_$p | tail -1 | cut -f1 >> "$output_dir/baseline_$p.log"
-
-    clear_caches
-
-    # Configure VPIC experiment
-    cd $deck_dir || die "cd failed"
-    mv $deck_dir/config.h $deck_dir/config.bkp || die "mv failed"
-    cat $deck_dir/config.bkp | \
-        sed 's/^\/\/#define VPIC_FILE_PER_PARTICLE/#define VPIC_FILE_PER_PARTICLE/' | \
-        sed 's/VPIC_TOPOLOGY_X.*/VPIC_TOPOLOGY_X '$CORES'/' | \
-        sed 's/VPIC_TOPOLOGY_Y.*/VPIC_TOPOLOGY_Y 1/' | \
-        sed 's/VPIC_TOPOLOGY_Z.*/VPIC_TOPOLOGY_Z 1/' | \
-        sed 's/VPIC_PARTICLE_X.*/VPIC_PARTICLE_X '$p'/' | \
-        sed 's/VPIC_PARTICLE_Y.*/VPIC_PARTICLE_Y '$p'/' | \
-        sed 's/VPIC_PARTICLE_Z.*/VPIC_PARTICLE_Z 1/' > $deck_dir/config.h || \
-        die "config.h editing failed"
-
-    # Run VPIC with DeltaFS
-    cd $output_dir || die "cd failed"
-    mkdir "$output_dir/deltafs_$p" || die "mkdir failed"
-    cd $output_dir/deltafs_$p || die "cd failed"
-
-    # TODO: Optimize and move to top
-    for mpi in openmpi mpich
-    do
-        which mpirun.${mpi}
-        if [ $? -eq 0 ]; then
-            MPI=$mpi
-            break
+    if [ `which mpirun.mpich` ]; then
+        if [ ${#envs[@]} -gt 0 ]; then
+            envstr="`printf "-env %s \"%s\" " ${envs[@]}`"
+        else
+            envstr=""
         fi
-    done
 
-    mkdir -p $output_dir/deltafs_$p/metadata || die "deltafs metadata mkdir failed"
-    mkdir -p $output_dir/deltafs_$p/data || die "deltafs data mkdir failed"
+        mpirun.mpich -np $procs --hostfile $output_dir/vpic.hosts \
+            $envstr -prepend-rank $opts $exe 2>&1 | tee "$outfile"
 
-    preload_lib_path="$umbrella_build_dir/deltafs-vpic-preload-prefix/src/"\
-"deltafs-vpic-preload-build/src/libdeltafs-preload.so"
-    deltafs_srvr_path="$umbrella_build_dir/deltafs-prefix/src/"\
-"deltafs-build/src/server/deltafs-srvr"
-    deltafs_srvr_ip=`hostname -i`
+    elif [ `which mpirun.openmpi` ]; then
+        if [ ${#envs[@]} -gt 0 ]; then
+            envstr="`printf "-x %s=\"%s\" " ${envs[@]}`"
+        else
+            envstr=""
+        fi
 
-    if [ x"$MPI" = xmpich ]; then
-        mpirun.mpich -np $CORES --hostfile $output_dir/vpic.hosts -prepend-rank \
-            -env LD_PRELOAD "$preload_lib_path" \
-            -env PRELOAD_Deltafs_root "particle" \
-            $deck_dir/turbulence.op 2>&1 | tee "$output_dir/deltafs_$p.log" || \
-            die "mpich run failed"
+        mpirun.openmpi -np $procs --hostfile $output_dir/vpic.hosts \
+            $envstr -tag-output $opts $exe 2>&1 | tee "$outfile"
 
-    elif [ x"$MPI" = xopenmpi ]; then
-        mpirun.openmpi -n 1 -tag-output \
-            -x DELTAFS_MetadataSrvAddrs="$deltafs_srvr_ip:10101" \
-            -x DELTAFS_FioName="posix" \
-            -x DELTAFS_FioConf="root=$output_dir/deltafs_$p/data" \
-            -x DELTAFS_Outputs="$output_dir/deltafs_$p/metadata" \
-        	$deltafs_srvr_path | tee "$output_dir/deltafs_srvr_$p.log" || \
-            die "openmpi run for deltafs server failed" &
-
-        srvr_pid=$!
-
-        mpirun.openmpi -np $CORES --hostfile $output_dir/vpic.hosts -tag-output \
-            -x LD_PRELOAD=$preload_lib_path \
-            -x PRELOAD_Deltafs_root=particle \
-            -x DELTAFS_MetadataSrvAddrs="$deltafs_srvr_ip:10101" \
-            -x SHUFFLE_Subnet=$ip_subnet \
-            $deck_dir/turbulence.op 2>&1 | tee "$output_dir/deltafs_$p.log" || \
-            (kill -KILL $srvr_pid && die "openmpi run failed")
-
-        kill -KILL $srvr_pid
     else
         die "not running mpich or openmpi"
     fi
+}
 
-    echo -n "Output size: " >> "$output_dir/deltafs_$p.log"
-    du -b $output_dir/deltafs_$p | tail -1 | cut -f1 >> "$output_dir/deltafs_$p.log"
+# Run VPIC
+# @1 in {"baseline", "deltafs"}
+# @2 number of particles
+do_run() {
+    runtype=$1
+    p=$2
+
+    message ""
+    message "=========================================================="
+    message "Running VPIC ($runtype) with $(( p * p * 100 )) particles."
+    message "=========================================================="
+    message ""
+
+    # Run VPIC experiment
+    cd $output_dir || die "cd failed"
+    mkdir "$output_dir/${runtype}_$p" || die "mkdir failed"
+    cd $output_dir/${runtype}_$p || die "cd failed"
+
+    case $runtype in
+    "baseline")
+        vars=()
+
+        do_mpirun $CORES vars[@] "" "$deck_dir/turbulence.op" \
+            "$output_dir/baseline_$p.log"
+        if [ $? -ne 0 ]; then
+            die "baseline: mpirun failed"
+        fi
+
+        echo -n "Output size: " >> "$output_dir/baseline_$p.log"
+        du -b $output_dir/baseline_$p | tail -1 | \
+            cut -f1 >> "$output_dir/baseline_$p.log"
+        ;;
+
+    "deltafs")
+        mkdir -p $output_dir/deltafs_$p/metadata || \
+            die "deltafs metadata mkdir failed"
+        mkdir -p $output_dir/deltafs_$p/data || \
+            die "deltafs data mkdir failed"
+
+        preload_lib_path="$umbrella_build_dir/deltafs-vpic-preload-prefix/src/"\
+"deltafs-vpic-preload-build/src/libdeltafs-preload.so"
+        deltafs_srvr_path="$umbrella_build_dir/deltafs-prefix/src/"\
+"deltafs-build/src/server/deltafs-srvr"
+        deltafs_srvr_ip=`hostname -i`
+
+        vars=("DELTAFS_MetadataSrvAddrs" "$deltafs_srvr_ip:10101"
+              "DELTAFS_FioName" "posix"
+              "DELTAFS_FioConf" "root=$output_dir/deltafs_$p/data"
+              "DELTAFS_Outputs" "$output_dir/deltafs_$p/metadata")
+
+        do_mpirun 1 vars[@] "" "$deltafs_srvr_path" \
+            "$output_dir/deltafs_srvr_$p.log"
+        if [ $? -ne 0 ]; then
+            die "deltafs server: mpirun failed"
+        fi
+
+        srvr_pid=$!
+
+        vars=("LD_PRELOAD" "$preload_lib_path"
+              "PRELOAD_Deltafs_root" "particle"
+              "DELTAFS_MetadataSrvAddrs" "$deltafs_srvr_ip:10101"
+              "SHUFFLE_Subnet" "$ip_subnet")
+
+        do_mpirun $CORES envs[@] "" "$deck_dir/turbulence.op" \
+            "$output_dir/deltafs_$p.log"
+        if [ $? -ne 0 ]; then
+            kill -KILL $srvr_pid
+            die "deltafs: mpirun failed"
+        fi
+
+        kill -KILL $srvr_pid
+
+        echo -n "Output size: " >> "$output_dir/deltafs_$p.log"
+        du -b $output_dir/deltafs_$p | tail -1 | \
+            cut -f1 >> "$output_dir/deltafs_$p.log"
+        ;;
+    esac
+
+}
+
+gen_hosts
+
+parts=$CORES
+while [ $dpoints -gt 0 ]
+do
+    clear_caches
+    build_deck "file-per-process"
+    do_run "baseline" $parts
+
+    clear_caches
+    build_deck "file-per-particle"
+    do_run "deltafs" $parts
 
     dpoints=$(( dpoints - 1 ))
     p=$(( p * 2 ))
