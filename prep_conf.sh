4bc7d191b45743b9fe291fc86fb2dca3d62d172e
George Amvrosiadis <gamvrosi@cs.cmu.edu>
2017-03-29 15:51:58 -0400

    Prep different conf files for MPICH and OpenMPI on Narwhal

--- prep_conf.sh
+++ prep_conf.sh
@@ -12,10 +12,13 @@ if [ $(hostname | grep tt-fey) ]; then
 elif [ $(hostname | grep tr-fe) ]; then
     echo "Preparing machine.conf for Trinity"
     cp $src_dir/trinitite.conf $src_dir/machine.conf
+elif [ $(hostname | grep narwhal) ] && [ `which mpirun.mpich` ]; then
+    echo "Preparing machine.conf for Narwhal (MPICH)"
+    cp $src_dir/narwhal-mpich.conf $src_dir/machine.conf
 elif [ $(hostname | grep narwhal) ]; then
-    echo "Preparing machine.conf for Narwhal"
-    cp $src_dir/narwhal.conf $src_dir/machine.conf
+    echo "Preparing machine.conf for Narwhal (OpenMPI)"
+    cp $src_dir/narwhal-openmpi.conf $src_dir/machine.conf
 else
     echo "Preparing default machine.conf"
-    cp $src_dir/narwhal.conf $src_dir/machine.conf
+    cp $src_dir/narwhal-openmpi.conf $src_dir/machine.conf
 fi
