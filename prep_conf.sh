4592ef76efbf1e7138c4810b0c0016a6463b523c
Chuck Cranor <chuck@ece.cmu.edu>
2017-10-09 12:25:43 -0400

    use CRAY_CPU_TARGET on the cray systems to choose a machine.conf. using the hostname does not work if you compile on the node because the hostnames are just node id number (nid00023).   furthermore, with the intel compiler you cannot easily build vpic for KNL on a haswell because the autoconf tries to compile and run programs and the intel toolchain doesn't want you to to that.
    so for compiling on KNL the thing to do is just submit it to a
    KNL node as a batch job...

--- prep_conf.sh
+++ prep_conf.sh
@@ -1,20 +1,32 @@
 #!/bin/bash
 #
 # Prepare a machine.conf for VPIC build
+#
+# for CRAY targets, let the $CRAY_CPU_TARGET take priority (since
+# the user should be setting the craype module target properly).
+# otherwise fall back to hostname.
+#
 
 src_dir="$1"
 host=$(hostname)
 
 rm -f $src_dir/machine.conf
-if [ $(hostname | grep tt-fey) ]; then
-    echo "Preparing machine.conf for Trinitite"
-    cp $src_dir/trinitite.conf $src_dir/machine.conf
+
+if [ x$CRAY_CPU_TARGET = xhaswell ]; then
+    echo "Preparing machine.conf for CRAY Haswell target"
+    cp $src_dir/cray-haswell.conf $src_dir/machine.conf
+elif [ x$CRAY_CPU_TARGET = xmic-knl ]; then
+    echo "Preparing machine.conf for CRAY MIC-KNL target"
+    cp $src_dir/cray-mic-knl.conf $src_dir/machine.conf
+elif [ $(hostname | grep tt-fey) ]; then
+    echo "Preparing machine.conf for Trinitite (haswell)"
+    cp $src_dir/cray-haswell.conf $src_dir/machine.conf
 elif [ $(hostname | grep tr-fe) ]; then
-    echo "Preparing machine.conf for Trinity"
-    cp $src_dir/trinitite.conf $src_dir/machine.conf
+    echo "Preparing machine.conf for Trinity (haswell)"
+    cp $src_dir/cray-haswell.conf $src_dir/machine.conf
 elif [ $(hostname | grep ga-fe) ]; then
-    echo "Preparing machine.conf for Gadget"
-    cp $src_dir/trinitite.conf $src_dir/machine.conf
+    echo "Preparing machine.conf for Gadget (haswell)"
+    cp $src_dir/cray-haswell.conf $src_dir/machine.conf
 elif [ $(hostname | grep narwhal) ] && [ `which mpirun.mpich` ]; then
     echo "Preparing machine.conf for Narwhal (MPICH)"
     cp $src_dir/narwhal-mpich.conf $src_dir/machine.conf
@@ -22,6 +34,9 @@ elif [ $(hostname | grep narwhal) ]; then
     echo "Preparing machine.conf for Narwhal (OpenMPI)"
     cp $src_dir/narwhal-openmpi.conf $src_dir/machine.conf
 else
+    echo "prep_conf.sh: WARNING: unknown system, using default"
     echo "Preparing default machine.conf"
     cp $src_dir/narwhal-openmpi.conf $src_dir/machine.conf
 fi
+
+exit 0
